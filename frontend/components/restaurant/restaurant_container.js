15724e902e0a4628e1be0b92ce5421a2dacfae06
jonathan-dwight <jd.buendia0711@gmail.com>
2020-10-19 10:23:41 -0700

    Add Restaurant Container

--- frontend/components/restaurant/restaurant_container.js
+++ frontend/components/restaurant/restaurant_container.js
@@ -1,4 +1,23 @@
 import { connect } from "react-redux";
-import * as RestaurantAPI from "../../util/restaurant_api_util"
+import { fetchRestaurant } from "../../actions/restaurant_actions"
+
+const mapStateToProps = (state, ownProps) => {
+    
+    let restaurant = state.entities.restaurants[ownProps.match.params.restaurantId]
+
+    return {
+        retaurant: restaurant
+    }
+
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+
+    return {
+        fetchRestaurant: (restaurantId) => dispatch(fetchRestaurant(restaurantId))
+    }
+}
+
 
 
