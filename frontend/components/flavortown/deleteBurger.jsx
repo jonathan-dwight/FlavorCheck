8f6fef93bded467f182a8ebe019053cee262c518
jonathan-dwight <jd.buendia0711@gmail.com>
2020-10-19 21:53:34 -0700

    Add CSS To Delete Button

--- frontend/components/flavortown/deleteBurger.jsx
+++ frontend/components/flavortown/deleteBurger.jsx
@@ -4,12 +4,20 @@ const removeBurger = (props) => {
     
     return(
         <>
-            <div>ARE YOU SURE YOU WANT TO REMOVE YOUR FLAVOR?? Your FlavorCheck 
-                will be gone forever!
+            <div className="delete-form">
+                <div className="delete-header">
+                    <h2 className="delete-header-text">Confirm Flavor Deletion</h2>
+                    <div onClick={props.closeModal} className="close-x-burger">X</div>
+                </div>
+                <div className="delete-text-box">
+                    <div className="delete-text">ARE YOU SURE YOU WANT TO REMOVE YOUR FLAVOR?? Your FlavorCheck 
+                        will be gone forever!
+                    </div>
+                </div>
+                <button className="delete-post-button" onClick={() => props.processForm(props.burgerId).then(()=> {
+                    props.closeModal()
+                })}>REMOVE FLAVOR :/</button>
             </div>
-            <button onClick={() => props.processForm(props.burgerId).then(()=> {
-                props.closeModal()
-            })}>Remove Flavor :/</button>
         </>
 
     )
