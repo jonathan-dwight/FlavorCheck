f7120b14bd077a0b5140cb258c763c227b63f6db
jonathan-dwight <jd.buendia0711@gmail.com>
2020-11-17 16:16:05 -0800

    Add Angelist

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const footerLinks = () => {
     
-    const linkedIn = "https://www.linkedin.com/in/jd-buendia-66ab7483/"
-    const github = "https://github.com/jonathan-dwight"
+    const linkedIn = "https://www.linkedin.com/in/jd-buendia-66ab7483/";
+    const github = "https://github.com/jonathan-dwight";
+    const angelList = "https://angel.co/u/jd-buendia";
 
     return (
         <>
@@ -15,6 +16,9 @@ const footerLinks = () => {
                     </a>
                     <a href={github} target="_blank">
                         <img src={window.github} className="footer-single" />
+                    </a>
+                    <a href={angelList} target="_blank">
+                        <img src={window.angelist} className="footer-single" />
                     </a>
                 </div>
             </div>
